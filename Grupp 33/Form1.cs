9c7925f19900da16772de56077002416431e0857
kalasjonte <81684659+kalasjonte@users.noreply.github.com>
2021-10-20 18:56:05 +0200

    Tillbaka på ruta 1

--- Grupp 33/Form1.cs
+++ Grupp 33/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace Grupp_33
         public Form1()
         {
             InitializeComponent();
+            RssController controller = new RssController();
         }
     }
 }
