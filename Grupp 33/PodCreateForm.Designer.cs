0666803550c152f7a679bdb5b5e9eca36b0c752f
ottupp <vilmaa.ottnerr@gmail.com>
2021-10-21 13:05:02 +0200

    jh

--- Grupp 33/PodCreateForm.Designer.cs
+++ Grupp 33/PodCreateForm.Designer.cs
@@ -29,12 +29,147 @@ namespace Grupp_33
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblHeadtitle = new System.Windows.Forms.Label();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblUpdFreq = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHeadtitle
+            // 
+            this.lblHeadtitle.AutoSize = true;
+            this.lblHeadtitle.Location = new System.Drawing.Point(262, 39);
+            this.lblHeadtitle.Name = "lblHeadtitle";
+            this.lblHeadtitle.Size = new System.Drawing.Size(290, 20);
+            this.lblHeadtitle.TabIndex = 0;
+            this.lblHeadtitle.Text = "Här kan du lägga till en podd till ditt feed";
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(160, 100);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(46, 20);
+            this.lblURL.TabIndex = 1;
+            this.lblURL.Text = "URL:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(164, 182);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(42, 20);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Titel:";
+            // 
+            // lblUpdFreq
+            // 
+            this.lblUpdFreq.AutoSize = true;
+            this.lblUpdFreq.Location = new System.Drawing.Point(458, 236);
+            this.lblUpdFreq.Name = "lblUpdFreq";
+            this.lblUpdFreq.Size = new System.Drawing.Size(169, 20);
+            this.lblUpdFreq.TabIndex = 3;
+            this.lblUpdFreq.Text = "Uppdateringsfrekvens:";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(223, 236);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(72, 20);
+            this.lblCategory.TabIndex = 4;
+            this.lblCategory.Text = "Kategori:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(227, 270);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(137, 28);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(462, 270);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(165, 28);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(227, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 26);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 26);
+            this.textBox2.TabIndex = 8;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(625, 386);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(130, 33);
+            this.btnCreate.TabIndex = 9;
+            this.btnCreate.Text = "Lägg till";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(44, 386);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 33);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Tillbaka";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // PodCreateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblUpdFreq);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.lblHeadtitle);
+            this.Name = "PodCreateForm";
             this.Text = "PodCreateForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblHeadtitle;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblUpdFreq;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
