d534ff648933f3a78fbabaa6a1d6454ae4779a91
Noel Lindgren <apexlindgren@hotmail.com>
2021-10-22 15:09:17 +0200

    exception klass

---
+++ BLL/UserException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class UserException : Exception
+    {
+        public UserException(string text) : base(text)
+        {
+        }
+        
+        public virtual bool SomethingThatShouldBeInt32(string intString)
+        {
+            bool correct = Int32.TryParse(intString, out int result);
+            if (!correct)
+            {
+                throw new UserException("");
+            }
+            return correct;
+        }
+
+
+    }
+}
