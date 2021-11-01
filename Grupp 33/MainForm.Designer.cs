dfe42853b16c6e0b64ac04618bacfbea840bb98c
kalasjonte <81684659+kalasjonte@users.noreply.github.com>
2021-11-01 17:12:12 +0100

    gang

--- Grupp 33/MainForm.Designer.cs
+++ Grupp 33/MainForm.Designer.cs
@@ -55,7 +55,6 @@ namespace Grupp_33
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.listViewCat = new System.Windows.Forms.ListView();
             this.KategoriNamn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.btnInfo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listViewPod
@@ -302,23 +301,11 @@ namespace Grupp_33
             // 
             this.KategoriNamn.Text = "Kategori namn";
             // 
-            // btnInfo
-            // 
-            this.btnInfo.Location = new System.Drawing.Point(21, 422);
-            this.btnInfo.Margin = new System.Windows.Forms.Padding(2);
-            this.btnInfo.Name = "btnInfo";
-            this.btnInfo.Size = new System.Drawing.Size(158, 40);
-            this.btnInfo.TabIndex = 22;
-            this.btnInfo.Text = "Information";
-            this.btnInfo.UseVisualStyleBackColor = true;
-            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1431, 793);
-            this.Controls.Add(this.btnInfo);
             this.Controls.Add(this.listViewCat);
             this.Controls.Add(this.listViewEp);
             this.Controls.Add(this.lblPodEp);
@@ -342,7 +329,6 @@ namespace Grupp_33
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "MainForm";
-            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -376,7 +362,6 @@ namespace Grupp_33
         private System.Windows.Forms.ColumnHeader Kategori;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader KategoriNamn;
-        private System.Windows.Forms.Button btnInfo;
     }
 }
 
